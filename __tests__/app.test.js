03dae60299ec41617c2eb6c884b68dab92576edb
austinxduong <axduong2@gmail.com>
2021-05-19 00:18:58 -0700

    location test munging function

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,35 +1,20 @@
-import app from '../lib/app.js';
-import supertest from 'supertest';
+// import app from '../lib/app.js';
+// import supertest from 'supertest';
 // import client from '../lib/client.js';
 // import { execSync } from 'child_process';
 
-const request = supertest(app);
+import { mungeLocation } from '../lib/munge-utils.js';
+import unmungedLocation from '../data/location-data';
+// const request = supertest(app);
+
+
 
 describe('API Routes', () => {
-
-
-
-  // If a GET request is made to /api/cats, does:
-  // 1) the server respond with status of 200
-  // 2) the body match the expected API data?
-  it('GET /api/cats', async () => {
-    // act - make the request
-    const response = await request.get('/api/cats');
-
-    // was response OK (200)?
-    expect(response.status).toBe(200);
-
-    // did it return the data we expected?
-    expect(response.body).toEqual(expectedCats);
-
-  });
-
-  // If a GET request is made to /api/cats/:id, does:
-  // 1) the server respond with status of 200
-  // 2) the body match the expected API data for the cat with that id?
-  test('GET /api/cats/:id', async () => {
-    const response = await request.get('/api/cats/2');
-    expect(response.status).toBe(200);
-    expect(response.body).toEqual(expectedCats[1]);
+  it('test munge location function', () => {
+    const expectation = {
+      formatted_query: 'Portland, Multnomah County, Oregon, USA', latitude: '45.5202471', longitude: '-122.6741949'
+    };
+    const result = mungeLocation(unmungedLocation);
+    expect(result).toEqual(expectation);
   });
 });
---
+++ data/location-data.js
@@ -0,0 +1,23 @@
+const unmungedLocation = [
+  {
+    'place_id': '282983083',
+    'licence': 'https://locationiq.com/attribution',
+    'osm_type': 'relation',
+    'osm_id': '186579',
+    'boundingbox': [
+      '45.432536',
+      '45.6528812',
+      '-122.8367489',
+      '-122.4720252'
+    ],
+    'lat': '45.5202471',
+    'lon': '-122.6741949',
+    'display_name': 'Portland, Multnomah County, Oregon, USA',
+    'class': 'place',
+    'type': 'city',
+    'importance': 0.75356571743377,
+    'icon': 'https://locationiq.org/static/images/mapicons/poi_place_city.p.20.png'
+  }
+];
+  
+export default unmungedLocation;
