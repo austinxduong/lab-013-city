b6ff4413682c33f5599ff04b542db72048d6b8c4
austinxduong <axduong2@gmail.com>
2021-05-22 23:32:02 -0700

    tucker helping me pass functions/munging on weather data

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,18 +3,50 @@
 // import client from '../lib/client.js';
 // import { execSync } from 'child_process';
 
-import { mungeLocation } from '../lib/munge-utils.js';
+import { mungeLocation, mungeWeather } from '../lib/munge-utils.js';
 import unmungedLocation from '../data/location-data';
+import unmungedWeather from '../data/weather-data';
 // const request = supertest(app);
 
 
 
 describe('API Routes', () => {
-  it('test munge location function', () => {
+  it('testing munge location function', () => {
+
     const expectation = {
       formatted_query: 'Portland, Multnomah County, Oregon, USA', latitude: '45.5202471', longitude: '-122.6741949'
+      
     };
+
     const result = mungeLocation(unmungedLocation);
+
     expect(result).toEqual(expectation);
+
   });
+
+  it('testing munge weather function', () => {
+
+    const expectation = 
+      [
+        {
+          'forecast': 'Broken clouds',
+          'time': '2021-05-22'
+        },
+        {
+          'forecast': 'Few clouds',
+          'time': '2021-05-23'
+        },
+
+      ];
+      
+  
+
+
+    const result = mungeWeather(unmungedWeather);
+
+
+    expect(result).toEqual(expectation);
+
+  });
+
 });
